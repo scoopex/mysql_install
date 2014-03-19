486b468ad840ced409998eded367f734273e24ec
Marc Schoechlin <ms-github@256bit.org>
2014-03-19 09:57:08 +0100

    n/a

--- backup-databases.sh
+++ backup-databases.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-
 #TODO:
 # - my.cnf backupppen
 # - letztes Backup aufheben
-# - fehlgeschlagene Bacjups entfernen
 
 BACKUPDIR="$1"
 MAXAGE="$2"
 
 TIMESTAMP="`date --date="today" "+%Y-%m-%d_%H-%M-%S"`"
+STARTTIME_GLOBAL="$SECONDS"
 
 sendStatus(){
     local STATUS="$1"
-    zabbix_sender -s `hostname` -c /etc/zabbix/zabbix_agentd.conf -k mysql.backup.globalstatus -o "$STATUS" -vvv
+    echo ">>>>$STATUS<<<<"
+    zabbix_sender -s `hostname` -c /etc/zabbix/zabbix_agentd.conf -k mysql.backup.globalstatus -o "$STATUS" > /dev/null
 }
+
+
+if [ -z "$BACKUPDIR" ] || [ -z "$MAXAGE" ];then
+	echo "USAGE: $0 <BACKUP PATH> <MAXAGE IN DAYS>"
+	exit 1
+fi
 
 cd $BACKUPDIR 
 if [ "$?" != 0 ];then
         echo "Unable to change to dir '$BACKUPDIR'"
 	exit 1 
 fi
+
+sendStatus "INFO: STARTING DATABASE BACKUP"
 
 FAILED="0"
 while read DBNAME;
@@ -30,22 +38,26 @@ do
         echo SKIPPED
 	continue
    fi
+   STARTTIME="$SECONDS"
    mysqldump --opt --triggers --routines --force --single-transaction "$DBNAME"|\
 	gzip -c > ${DBNAME}-${TIMESTAMP}_currently_dumping.sql.gz
    RET="$?"
+
+   DURATION="$(( $(( $SECONDS - $STARTTIME )) / 60 ))"
    if [ "$RET" == "0" ];then
-	echo "SUCESSFULLY CREATED BACKUP FOR '$DBNAME'"
+        sendStatus "INFO: SUCESSFULLY CREATED BACKUP FOR '$DBNAME' in $DURATION minutes"
 	mv ${DBNAME}-${TIMESTAMP}_currently_dumping.sql.gz ${DBNAME}-${TIMESTAMP}.sql.gz
    else
 	FAILED="$($FAILED + 1)"
-	echo "FAILED TO BACKUP '$DBNAME'"
+        sendStatus "INFO: FAILED TO BACKUP '$DBNAME'  in $DURATION minutes"
    fi
 done < <(mysql --xml -e "show databases;"|grep '<field name="Database">'|sed '~s,^.*<field name="Database">\(..*\)</field>.*$,\1,')
 
+DURATION="$(( $(( $SECONDS - $STARTTIME_GLOBAL )) / 60 ))"
 if [ "$FAILED" -gt 0 ];then 
-  echo "OVERALL STATUS: FAILED ($FAILED backups)"
+  sendStatus "ERROR: FAILED ($FAILED failed backups, in $DURATION minutes)"
 else
-  echo "OVERALL STATUS: SUCCESS"
+  sendStatus "OK: BACKUPS WERE SUCCESSFUL ($DURATION minutes)"
 fi
 
 echo "*** REMOVE OUTDATED BACKUPS **********************************************************************"
@@ -57,4 +69,4 @@ else
 	exit 1 
 fi
 
-
+echo "TOTAL AMOUNT OF BACKUPS $( du -scmh *.sql.gz|awk '/total/{print $1}')"
