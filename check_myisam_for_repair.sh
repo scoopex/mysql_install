9aec37635ebe4ba20aeef307702c06921d41e5f5
Marc Schoechlin <marc.schoechlin@vico-research.com>
2020-01-17 11:10:33 +0100

    rename

--- check_myisam_for_repair.sh
+++ check_myisam_for_repair.sh
@@ -7,21 +7,29 @@ exec_query(){
 
 DATABASES="$(exec_query 'show databases'|grep -v -P "performance_schema|information_schema" |xargs)"
 
-rm -v /tmp/repair.sql
+TMPFILE="$(mktemp /tmp/repair-XXXXXXXXX.sql)"
+
+echo "REPAIR FILE $TMPFILE"
+
 for DATABASE in $DATABASES;
 do
   echo "==> $DATABASE"
   exec_query "show tables FROM $DATABASE"|while read TABLE; do
+    exec_query "show create table ${DATABASE}.${TABLE}" | grep -q "ENGINE=MyISAM"
+    if [ "$?" != "0" ];then
+      continue
+    fi
+
     echo -n "."
     exec_query "select * from ${DATABASE}.${TABLE} limit 10" >/dev/null
     if [ "$?" != "0" ];then
       echo
       echo "BROKEN ${DATABASE}.${TABLE} (see /tmp/repair.sql)"
-      echo "REPAIR TABLE ${DATABASE}.${TABLE} EXTENDED;" >>/tmp/repair.sql
+      echo "REPAIR TABLE ${DATABASE}.${TABLE} EXTENDED;" >>$TMPFILE
     fi
   done
   echo 
 done
 
-echo "INFO: identified all crashed tables"
-echo "mysql -c /tmp/repair.sql"
+echo "INFO: completed to identify all crashed tables"
+echo "mysql < $TMPFILE"
